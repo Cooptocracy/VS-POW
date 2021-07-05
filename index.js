7cf26a08e38731ed5f0f100f421bc2112fca707f
srihari kapu <sriharikapu163@gmail.com>
2021-07-06 00:31:35 +0530

    simple implemenation

---
+++ index.js
@@ -0,0 +1,16 @@
+const fork = require('child_process').fork;
+const pow = fork('./consensus/pow.js');
+
+pow.send({
+  data: 'data to test', // Required String or Buffer
+  difficulty: 4, // Optional (default 6)
+  alg: 'sha3-256' // Optional hashing algorithm (default sha3-256)
+});
+
+console.log('POW started ...');
+
+pow.on('message', msg => {
+  console.log('Hash:', msg.hash);
+  console.log('Nonce:', msg.nonce);
+  pow.kill();
+});
